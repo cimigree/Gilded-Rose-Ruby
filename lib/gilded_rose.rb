04b35a60acff458619c4300a8f315ba4e3436344
Cindy Green <cimigreen@hotmail.com>
2018-05-17 15:56:26 -0400

    Reorders methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -12,7 +12,7 @@ class GildedRose
       if @days_remaining > 0
         change_quality_before_sell_by
       else 
-        change_quality_after_sell_date
+        change_quality_after_sell_by
       end
       @days_remaining = @days_remaining - 1
       return_correct_range!
@@ -21,38 +21,37 @@ class GildedRose
 
   private 
 
-  def return_correct_range!
-      @quality = 0 if @quality < 0
-      @quality = 50 if @quality > 50
-  end
-
-
   def change_quality_before_sell_by
     if quality_decreases_over_time?
       @quality = @quality - 1
       if conjured?
-        @quality = @quality-1
+        @quality = @quality - 1
       end
     else
       increase_quality
     end
   end
   
-  def change_quality_after_sell_date
+  def change_quality_after_sell_by
     if quality_decreases_over_time?
       @quality = @quality - 2
       if conjured?
       @quality = @quality - 2
       end
     elsif backstage_pass?
-      @quality = @quality - @quality 
+      @quality = 0 
     else
       @quality = @quality + 2 
     end
   end
 
-  def backstage_pass?
-    @name == "Backstage passes to a TAFKAL80ETC concert"
+  def return_correct_range!
+    @quality = 0 if @quality < 0
+    @quality = 50 if @quality > 50
+  end
+
+  def quality_decreases_over_time?
+    @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
   end
 
   def conjured?
@@ -66,6 +65,10 @@ class GildedRose
     end
   end
 
+  def backstage_pass?
+    @name == "Backstage passes to a TAFKAL80ETC concert"
+  end
+
   def increase_quality_for_backstage_pass
     if @days_remaining < 11
       @quality = @quality + 1
@@ -73,9 +76,5 @@ class GildedRose
     if @days_remaining < 6
       @quality = @quality + 1
     end
-  end
-
-  def quality_decreases_over_time?
-    @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
   end
 end
