4159e110f0db46bdaf4cdbcef7d891b512298531
Cindy Green <cimigreen@hotmail.com>
2018-05-17 15:28:38 -0400

    Tests pass

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,40 +9,67 @@ class GildedRose
 
   def tick
     if @name != "Sulfuras, Hand of Ragnaros"
-      before_sell_by
+      if @days_remaining > 0
+        change_quality_before_sell_by
+      else 
+        change_quality_after_sell_date
+      end
       @days_remaining = @days_remaining - 1
-    end
-    if @days_remaining < 0
-      after_sell_date
     end
   end
 
   private 
-  
-  def after_sell_date
-    if @name != "Aged Brie"
-      if @name != "Backstage passes to a TAFKAL80ETC concert"
-        if @quality > 0
-          if @name == "Conjured Mana Cake"
-            @quality = @quality - 2
-          elsif @name != "Sulfuras, Hand of Ragnaros"
-            @quality = @quality - 1
+
+  def change_quality_before_sell_by
+    if quality_decreases_over_time?
+      if @quality > 0
+        @quality = @quality - 1
+        if conjured?
+          if @quality > 0
+            @quality = @quality-1
           end
         end
-      else
-        @quality = @quality - @quality
       end
     else
+      increase_quality
+    end
+  end
+  
+  def change_quality_after_sell_date
+    if quality_decreases_over_time?
+      if @quality >= 2
+        @quality = @quality - 2
+        if conjured?
+          if @quality >= 2
+            @quality = @quality - 2
+          end
+        end
+      end
+    elsif backstage_pass?
+        @quality = @quality - @quality 
+    else
+      if @quality < 50
+        @quality = @quality + 1 
+      end 
       if @quality < 50
         @quality = @quality + 1
       end
     end
   end
 
-  def quality_increases
+  def backstage_pass?
+    @name == "Backstage passes to a TAFKAL80ETC concert"
+  end
+
+  def conjured?
+    !(@name.downcase =~ /^conj/).nil?
+  end
+
+
+  def increase_quality
     if @quality < 50
       @quality = @quality + 1
-      if @name == "Backstage passes to a TAFKAL80ETC concert"
+      if backstage_pass?
         if @days_remaining < 11
           if @quality < 50
             @quality = @quality + 1
@@ -58,21 +85,9 @@ class GildedRose
   end
 
 
-  def before_sell_by
-    if quality_decreases?
-      if @quality > 0
-        if @name == "Conjured Mana Cake"
-          @quality = @quality - 2
-        else
-          @quality = @quality - 1
-        end
-      end
-    else
-      quality_increases(@name, @quality)
-    end
-  end
+  
 
-  def quality_decreases?
+  def quality_decreases_over_time?
     @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
   end
 end
