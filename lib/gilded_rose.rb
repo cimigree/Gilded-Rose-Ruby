fb5f1e936dd7757354d52ee2a96e85a13400732e
cimigree <cimigreen@hotmail.com>
2018-10-21 16:00:45 -0400

    A different approach -- by item instead of behavior

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -5,51 +5,47 @@ class GildedRose
     @name = name
     @days_remaining = days_remaining
     @quality = quality
+    @unchangeable_item if @name == "Sulfuras, Hand of Ragnaros"
   end
 
   def tick
-    if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
-      if @quality > 0
-        if @name != "Sulfuras, Hand of Ragnaros"
-          @quality = @quality - 1
-        end
-      end
+    return if @unchangeable_item
+    age_item
+    @quality = 0 if @quality < 0
+    @quality = 50 if @quality >= 50
+    @days_remaining -= 1
+  end
+
+
+  def age_item
+    case @name
+      when "Aged Brie" then age_brie
+      when "Backstage passes to a TAFKAL80ETC concert" then age_backstage_passes
+      when "Conjured Mana Cake" then age_conjured
+      else age_normal_item
+    end
+  end
+
+  def age_brie
+    @days_remaining > 0 ? @quality += 1 : @quality += 2
+  end
+
+  def age_backstage_passes
+    if @days_remaining <= 0
+      return @quality = 0 
     else
-      if @quality < 50
-        @quality = @quality + 1
-        if @name == "Backstage passes to a TAFKAL80ETC concert"
-          if @days_remaining < 11
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-          if @days_remaining < 6
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-        end
-      end
+      @quality += 1
+      @quality += 1 if @days_remaining <= 10
+      @quality += 1 if @days_remaining <= 5
     end
-    if @name != "Sulfuras, Hand of Ragnaros"
-      @days_remaining = @days_remaining - 1
-    end
-    if @days_remaining < 0
-      if @name != "Aged Brie"
-        if @name != "Backstage passes to a TAFKAL80ETC concert"
-          if @quality > 0
-            if @name != "Sulfuras, Hand of Ragnaros"
-              @quality = @quality - 1
-            end
-          end
-        else
-          @quality = @quality - @quality
-        end
-      else
-        if @quality < 50
-          @quality = @quality + 1
-        end
-      end
-    end
+  end
+
+  def age_conjured
+    age_normal_item
+    age_normal_item
+  end
+
+  def age_normal_item
+    @days_remaining > 0 ? @quality -= 1 : @quality -= 2
   end
 end
